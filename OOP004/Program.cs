204faf0ee486dcce20f8d2b8b525507e2e8d50c8
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-17 23:51:09 +0300

    question1

--- OOP004/Program.cs
+++ OOP004/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace OOP004
+﻿using Microsoft.VisualBasic;
+using System.Numerics;
+
+namespace OOP004
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question1
+
+            //            Q1
+            //(A)Abstraction is hiding the complex implementation details and showing only the important information or functionality to the user
+
+            //(b)Because it helps us focus on what an object does instead of how it does it, which makes
+            //the code easier to understand and maintain
+
+            #endregion
         }
     }
 }
