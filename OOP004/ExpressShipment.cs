d9b83264a3d82a7dafffd075e8f3341d876b1781
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-18 04:15:51 +0300

    Expressshipment

--- OOP004/ExpressShipment.cs
+++ OOP004/ExpressShipment.cs
@@ -4,64 +4,64 @@ using System.Text;
 
 #region Expressshipment
 
-namespace OOP004
-{
-    public class ExpressShipment : Shipment, ITrackable, IInsurable
-    {
-        private decimal extraFee;
+//namespace OOP004
+//{
+//    public class ExpressShipment : Shipment, ITrackable, IInsurable
+//    {
+//        private decimal extraFee;
 
-        public decimal ExtraFee
-        {
-            get { return extraFee; }
-            set
-            {
-                if (value >= 0)
-                    extraFee = value;
-            }
-        }
+//        public decimal ExtraFee
+//        {
+//            get { return extraFee; }
+//            set
+//            {
+//                if (value >= 0)
+//                    extraFee = value;
+//            }
+//        }
 
-        public ExpressShipment(
-            string trackingCode,
-            string description,
-            decimal weight,
-            decimal deliveryFee,
-            DeliveryAddress destination,
-            decimal extraFee)
-            : base(trackingCode, description, weight, deliveryFee, destination)
-        {
-            ExtraFee = extraFee;
-        }
+//        public ExpressShipment(
+//            string trackingCode,
+//            string description,
+//            decimal weight,
+//            decimal deliveryFee,
+//            DeliveryAddress destination,
+//            decimal extraFee)
+//            : base(trackingCode, description, weight, deliveryFee, destination)
+//        {
+//            ExtraFee = extraFee;
+//        }
 
-        public override decimal EstimatedCost
-        {
-            get
-            {
-                return DeliveryFee + (Weight * 5) + ExtraFee;
-            }
-        }
+//        public override decimal EstimatedCost
+//        {
+//            get
+//            {
+//                return DeliveryFee + (Weight * 5) + ExtraFee;
+//            }
+//        }
 
-        public override void PrintShipment()
-        {
-            Console.WriteLine("Express Shipment");
-            Console.WriteLine($"Tracking Code : {TrackingCode}");
-            Console.WriteLine($"Description : {Description}");
-            Console.WriteLine($"Weight : {Weight} KG");
-            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
-            Console.WriteLine($"Extra Fee : {ExtraFee} EGP");
-            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
-        }
+//        public override void PrintShipment()
+//        {
+//            Console.WriteLine("Express Shipment");
+//            Console.WriteLine($"Tracking Code : {TrackingCode}");
+//            Console.WriteLine($"Description : {Description}");
+//            Console.WriteLine($"Weight : {Weight} KG");
+//            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
+//            Console.WriteLine($"Extra Fee : {ExtraFee} EGP");
+//            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
+//        }
 
-        public string GetTrackingStatus()
-        {
-            return $"Shipment {TrackingCode} is Out for Delivery.";
-        }
+//        public string GetTrackingStatus()
+//        {
+//            return $"Shipment {TrackingCode} is Out for Delivery.";
+//        }
 
-        public decimal CalculateInsurance()
-        {
-            return EstimatedCost * 0.08m;
-        }
-    }
-}
+//        public decimal CalculateInsurance()
+//        {
+//            return EstimatedCost * 0.08m;
+//        }
+//    }
+//}
 
 
 
