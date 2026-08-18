bfb1ad68bfe9f0356c93f360aaa45b4a7d2bac42
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-18 03:50:47 +0300

    Deliveryaddress

--- OOP004/DeliveryAddress.cs
+++ OOP004/DeliveryAddress.cs
@@ -4,27 +4,27 @@ using System.Text;
 
 #region Deliveryaddress
 
-namespace OOP004
-{
-    public struct DeliveryAddress
-    {
-        public string City;
-        public string Street;
-        public int BuildingNumber;
+//namespace OOP004
+//{
+//    public struct DeliveryAddress
+//    {
+//        public string City;
+//        public string Street;
+//        public int BuildingNumber;
 
-        public DeliveryAddress(string city, string street, int buildingNumber)
-        {
-            City = city;
-            Street = street;
-            BuildingNumber = buildingNumber;
-        }
+//        public DeliveryAddress(string city, string street, int buildingNumber)
+//        {
+//            City = city;
+//            Street = street;
+//            BuildingNumber = buildingNumber;
+//        }
 
-        public string GetFullAddress()
-        {
-            return $"{BuildingNumber} {Street}, {City}";
-        }
-    }
-}
+//        public string GetFullAddress()
+//        {
+//            return $"{BuildingNumber} {Street}, {City}";
+//        }
+//    }
+//}
 
 
 
