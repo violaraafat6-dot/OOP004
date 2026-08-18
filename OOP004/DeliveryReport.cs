31221efc93ddb4b737a889331448c7414f39595f
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-18 04:08:49 +0300

    Deliveryreport

--- OOP004/DeliveryReport.cs
+++ OOP004/DeliveryReport.cs
@@ -4,20 +4,20 @@ using System.Text;
 
 #region Deliveryreport
 
-namespace OOP004
-{
-    public static class DeliveryReport
-    {
-        public static void PrintShipment(ITrackable shipment)
-        {
-            Console.WriteLine(shipment.GetTrackingStatus());
-        }
+//namespace OOP004
+//{
+//    public static class DeliveryReport
+//    {
+//        public static void PrintShipment(ITrackable shipment)
+//        {
+//            Console.WriteLine(shipment.GetTrackingStatus());
+//        }
 
-        public static void PrintInsurance(IInsurable shipment)
-        {
-            Console.WriteLine($"Insurance : {shipment.CalculateInsurance():0.00} EGP");
-        }
-    }
-}
+//        public static void PrintInsurance(IInsurable shipment)
+//        {
+//            Console.WriteLine($"Insurance : {shipment.CalculateInsurance():0.00} EGP");
+//        }
+//    }
+//}
 
 #endregion
