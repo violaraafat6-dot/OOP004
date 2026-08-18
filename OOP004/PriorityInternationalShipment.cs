885f5b1d357c221983eeecf2ec52651d14d56ccb
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-18 04:35:25 +0300

    priority

--- OOP004/PriorityInternationalShipment.cs
+++ OOP004/PriorityInternationalShipment.cs
@@ -4,35 +4,35 @@ using System.Text;
 
 #region priority 
 
-namespace OOP004
-{
-    public class PriorityInternationalShipment : InternationalShipment
-    {
-        public PriorityInternationalShipment(
-            string trackingCode,
-            string description,
-            decimal weight,
-            decimal deliveryFee,
-            DeliveryAddress destination,
-            string destinationCountry,
-            decimal customsFee)
-            : base(
-                trackingCode,
-                description,
-                weight,
-                deliveryFee,
-                destination,
-                destinationCountry,
-                customsFee)
-        {
-        }
+//namespace OOP004
+//{
+//    public class PriorityInternationalShipment : InternationalShipment
+//    {
+//        public PriorityInternationalShipment(
+//            string trackingCode,
+//            string description,
+//            decimal weight,
+//            decimal deliveryFee,
+//            DeliveryAddress destination,
+//            string destinationCountry,
+//            decimal customsFee)
+//            : base(
+//                trackingCode,
+//                description,
+//                weight,
+//                deliveryFee,
+//                destination,
+//                destinationCountry,
+//                customsFee)
+//        {
+//        }
 
-        public sealed override void GenerateCustomsReport()
-        {
-            Console.WriteLine($"Priority Customs Report for {DestinationCountry}");
-        }
-    }
-}
+//        public sealed override void GenerateCustomsReport()
+//        {
+//            Console.WriteLine($"Priority Customs Report for {DestinationCountry}");
+//        }
+//    }
+//}
 
 
 
