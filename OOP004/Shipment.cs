32dbb4b9edc84728d7826127ecf3d0cadfb71a9b
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-18 04:36:42 +0300

    Shipment

--- OOP004/Shipment.cs
+++ OOP004/Shipment.cs
@@ -4,102 +4,102 @@ using System.Text;
 
 #region Shipment
 
-namespace OOP004
-{
-    public abstract class Shipment
-    {
-        private string trackingCode;
-        private string description;
-        private decimal weight;
-        private decimal deliveryFee;
+//namespace OOP004
+//{
+//    public abstract class Shipment
+//    {
+//        private string trackingCode;
+//        private string description;
+//        private decimal weight;
+//        private decimal deliveryFee;
 
-        public DeliveryAddress Destination { get; set; }
+//        public DeliveryAddress Destination { get; set; }
 
-        public string TrackingCode
-        {
-            get { return trackingCode; }
-        }
+//        public string TrackingCode
+//        {
+//            get { return trackingCode; }
+//        }
 
-        public string Description
-        {
-            get { return description; }
-            set
-            {
-                if (!string.IsNullOrWhiteSpace(value))
-                    description = value;
-            }
-        }
+//        public string Description
+//        {
+//            get { return description; }
+//            set
+//            {
+//                if (!string.IsNullOrWhiteSpace(value))
+//                    description = value;
+//            }
+//        }
 
-        public decimal Weight
-        {
-            get { return weight; }
-            set
-            {
-                if (value > 0)
-                    weight = value;
-            }
-        }
+//        public decimal Weight
+//        {
+//            get { return weight; }
+//            set
+//            {
+//                if (value > 0)
+//                    weight = value;
+//            }
+//        }
 
-        public decimal DeliveryFee
-        {
-            get { return deliveryFee; }
-            private set
-            {
-                if (value > 0)
-                    deliveryFee = value;
-            }
-        }
+//        public decimal DeliveryFee
+//        {
+//            get { return deliveryFee; }
+//            private set
+//            {
+//                if (value > 0)
+//                    deliveryFee = value;
+//            }
+//        }
 
-        public abstract decimal EstimatedCost { get; }
+//        public abstract decimal EstimatedCost { get; }
 
-        public Shipment(string trackingCode)
-        {
-            if (!string.IsNullOrWhiteSpace(trackingCode))
-                this.trackingCode = trackingCode;
+//        public Shipment(string trackingCode)
+//        {
+//            if (!string.IsNullOrWhiteSpace(trackingCode))
+//                this.trackingCode = trackingCode;
 
-            Description = "Unknown";
-            Weight = 1;
-            DeliveryFee = 50;
-            Destination = new DeliveryAddress("Unknown", "Unknown", 0);
-        }
+//            Description = "Unknown";
+//            Weight = 1;
+//            DeliveryFee = 50;
+//            Destination = new DeliveryAddress("Unknown", "Unknown", 0);
+//        }
 
-        public Shipment(
-            string trackingCode,
-            string description,
-            decimal weight,
-            decimal deliveryFee,
-            DeliveryAddress destination)
-        {
-            if (!string.IsNullOrWhiteSpace(trackingCode))
-                this.trackingCode = trackingCode;
+//        public Shipment(
+//            string trackingCode,
+//            string description,
+//            decimal weight,
+//            decimal deliveryFee,
+//            DeliveryAddress destination)
+//        {
+//            if (!string.IsNullOrWhiteSpace(trackingCode))
+//                this.trackingCode = trackingCode;
 
-            Description = description;
-            Weight = weight;
-            DeliveryFee = deliveryFee;
-            Destination = destination;
-        }
+//            Description = description;
+//            Weight = weight;
+//            DeliveryFee = deliveryFee;
+//            Destination = destination;
+//        }
 
-        public void UpdateDeliveryFee(decimal newFee)
-        {
-            if (newFee > 0)
-                DeliveryFee = newFee;
-        }
+//        public void UpdateDeliveryFee(decimal newFee)
+//        {
+//            if (newFee > 0)
+//                DeliveryFee = newFee;
+//        }
 
-        public void UpdateWeight(decimal newWeight)
-        {
-            if (newWeight > 0)
-                Weight = newWeight;
-        }
+//        public void UpdateWeight(decimal newWeight)
+//        {
+//            if (newWeight > 0)
+//                Weight = newWeight;
+//        }
 
-        public void UpdateWeight(decimal newWeight, decimal packingWeight)
-        {
-            if (newWeight > 0 && packingWeight >= 0)
-                Weight = newWeight + packingWeight;
-        }
+//        public void UpdateWeight(decimal newWeight, decimal packingWeight)
+//        {
+//            if (newWeight > 0 && packingWeight >= 0)
+//                Weight = newWeight + packingWeight;
+//        }
 
-        public abstract void PrintShipment();
-    }
-}
+//        public abstract void PrintShipment();
+//    }
+//}
 
 
 
