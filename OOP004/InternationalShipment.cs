a619ffcae5d5d5f8aff40757c85a5cdfc44aae0d
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-18 04:25:08 +0300

    international

--- OOP004/InternationalShipment.cs
+++ OOP004/InternationalShipment.cs
@@ -4,82 +4,82 @@ using System.Text;
 
 #region international
 
-namespace OOP004
-{
-    public class InternationalShipment : Shipment, ITrackable, IInsurable
-    {
-        private string destinationCountry;
-        private decimal customsFee;
+//namespace OOP004
+//{
+//    public class InternationalShipment : Shipment, ITrackable, IInsurable
+//    {
+//        private string destinationCountry;
+//        private decimal customsFee;
 
-        public string DestinationCountry
-        {
-            get { return destinationCountry; }
-            set
-            {
-                if (!string.IsNullOrWhiteSpace(value))
-                    destinationCountry = value;
-            }
-        }
+//        public string DestinationCountry
+//        {
+//            get { return destinationCountry; }
+//            set
+//            {
+//                if (!string.IsNullOrWhiteSpace(value))
+//                    destinationCountry = value;
+//            }
+//        }
 
-        public decimal CustomsFee
-        {
-            get { return customsFee; }
-            set
-            {
-                if (value >= 0)
-                    customsFee = value;
-            }
-        }
+//        public decimal CustomsFee
+//        {
+//            get { return customsFee; }
+//            set
+//            {
+//                if (value >= 0)
+//                    customsFee = value;
+//            }
+//        }
 
-        public InternationalShipment(
-            string trackingCode,
-            string description,
-            decimal weight,
-            decimal deliveryFee,
-            DeliveryAddress destination,
-            string destinationCountry,
-            decimal customsFee)
-            : base(trackingCode, description, weight, deliveryFee, destination)
-        {
-            DestinationCountry = destinationCountry;
-            CustomsFee = customsFee;
-        }
+//        public InternationalShipment(
+//            string trackingCode,
+//            string description,
+//            decimal weight,
+//            decimal deliveryFee,
+//            DeliveryAddress destination,
+//            string destinationCountry,
+//            decimal customsFee)
+//            : base(trackingCode, description, weight, deliveryFee, destination)
+//        {
+//            DestinationCountry = destinationCountry;
+//            CustomsFee = customsFee;
+//        }
 
-        public override decimal EstimatedCost
-        {
-            get
-            {
-                return DeliveryFee + (Weight * 5) + CustomsFee;
-            }
-        }
+//        public override decimal EstimatedCost
+//        {
+//            get
+//            {
+//                return DeliveryFee + (Weight * 5) + CustomsFee;
+//            }
+//        }
 
-        public override void PrintShipment()
-        {
-            Console.WriteLine("International Shipment");
-            Console.WriteLine($"Tracking Code : {TrackingCode}");
-            Console.WriteLine($"Description : {Description}");
-            Console.WriteLine($"Weight : {Weight} KG");
-            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
-            Console.WriteLine($"Destination Country : {DestinationCountry}");
-            Console.WriteLine($"Customs Fee : {CustomsFee} EGP");
-            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
-        }
+//        public override void PrintShipment()
+//        {
+//            Console.WriteLine("International Shipment");
+//            Console.WriteLine($"Tracking Code : {TrackingCode}");
+//            Console.WriteLine($"Description : {Description}");
+//            Console.WriteLine($"Weight : {Weight} KG");
+//            Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
+//            Console.WriteLine($"Destination Country : {DestinationCountry}");
+//            Console.WriteLine($"Customs Fee : {CustomsFee} EGP");
+//            Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
+//        }
 
-        public string GetTrackingStatus()
-        {
-            return $"Shipment {TrackingCode} has been Delivered.";
-        }
+//        public string GetTrackingStatus()
+//        {
+//            return $"Shipment {TrackingCode} has been Delivered.";
+//        }
 
-        public decimal CalculateInsurance()
-        {
-            return EstimatedCost * 0.12m;
-        }
+//        public decimal CalculateInsurance()
+//        {
+//            return EstimatedCost * 0.12m;
+//        }
 
-        public virtual void GenerateCustomsReport()
-        {
-            Console.WriteLine($"Customs Report for {DestinationCountry}");
-        }
-    }
-}
+//        public virtual void GenerateCustomsReport()
+//        {
+//            Console.WriteLine($"Customs Report for {DestinationCountry}");
+//        }
+//    }
+//}
 
 #endregion
